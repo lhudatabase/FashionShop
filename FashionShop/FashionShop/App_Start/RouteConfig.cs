d27f9c41f53812ce9c2ddce56d304979895ac681
thiennguyenvu <thiennguyenvu1996@gmail.com>
2018-05-17 15:00:38 +0700

    created and set route

---
+++ FashionShop/FashionShop/App_Start/RouteConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FashionShop
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            #region ShopController
+            routes.MapRoute(
+               name: "Products",
+               url: "products",
+               defaults: new { controller = "Shop", action = "Products", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "Services",
+               url: "services",
+               defaults: new { controller = "Shop", action = "Services", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "Contact",
+               url: "contact",
+               defaults: new { controller = "Shop", action = "Contact", id = UrlParameter.Optional }
+           );
+            #endregion
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FashionShop/FashionShop/Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FashionShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
